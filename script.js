52d57512cc677037810f1bf8943314e35c8a1cb7
bhavya092 <bhavyamehta922@gmail.com>
2020-12-18 18:31:19 +0530

    All files

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i=0; i< 10;i++) {
+    var x= names[i];
+    var a=x[0];
+
+
+  if ( a==='j' || a==='J') {
+     console.log("Good Bye "+ x)
+  } 
+  else {
+   console.log("Hello " + x)
+  }
+}
